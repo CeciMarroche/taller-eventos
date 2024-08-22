06be2bb9f3413a0c41b183501947050bfc19df36
CeciMarroche <68209222+CeciMarroche@users.noreply.github.com>
2024-08-22 13:58:07 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -3,3 +3,10 @@ var div = document.getElementById('div')
 div.addEventListener('click',function() {
     alert('Hola! Soy el div');
 });
+
+var div = document.getElementById('boton')
+
+div.addEventListener('click',function(event) {
+    alert('Hola!);
+    event.stopPropagation(); 
+});
